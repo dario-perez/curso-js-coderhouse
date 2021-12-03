b1e6bdc1ea95d4d03326caaaa6519cc8b22b66ca
Dario <dario.nic30@gmail.com>
2021-12-03 02:01:47 -0300

    Subido ejercicio de clase 09/10

---
+++ clase-09-10/scripts/products.js
@@ -0,0 +1,38 @@
+let products = [
+  {
+    name: 'Smart TV',
+    id: 01,
+    price: 50000,
+    stock: 42,
+  },
+  {
+    name: 'Laptop',
+    id: 02,
+    price: 90000,
+    stock: 36,
+  },
+  {
+    name: 'Smartphone',
+    id: 03,
+    price: 60500,
+    stock: 10,
+  },
+  {
+    name: 'Monitor',
+    id: 04,
+    price: 32000,
+    stock: 13,
+  },
+  {
+    name: 'Impresora',
+    id: 05,
+    price: 25000,
+    stock: 25,
+  },
+  {
+    name: 'Home Theater',
+    id: 06,
+    price: 42000,
+    stock: 20,
+  },
+];
